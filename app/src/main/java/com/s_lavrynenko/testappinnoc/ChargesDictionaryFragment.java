d7fe648b079914ec3939687c42ca3e5b4193bc4f
Sergey Lavrynenko <s.o.lavrynenko@gmail.com>
2016-08-10 11:04:54 +0300

    just one fragment and menu

---
+++ app/src/main/java/com/s_lavrynenko/testappinnoc/ChargesDictionaryFragment.java
@@ -0,0 +1,25 @@
+package com.s_lavrynenko.testappinnoc;
+
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+
+public class ChargesDictionaryFragment extends ListFragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                inflater.getContext(),
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.charges_dictionary));
+        setListAdapter(adapter);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
